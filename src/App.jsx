543abf925bdec1629b4fb48836d9357f3621182d
Javier Perez <fabrimotor2026@gmail.com>
2026-06-09 15:11:18 +0200

    Fix savedStartupPiece reference

--- src/App.jsx
+++ src/App.jsx
@@ -910,7 +910,7 @@ export default function App() {
         lote: savedStartupLot || previous.lote || "",
       }));
 
-      if (isVerificationUser(currentUser) && !savedStartupPiece) {
+      if (isVerificationUser(currentUser) && !localStorage.getItem("startupReference")) {
         setShowProductionStart(true);
       }
 
