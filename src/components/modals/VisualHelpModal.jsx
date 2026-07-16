f1685c8a8b682b14031f61a5f5cea7cf0a56d3f7
Javier Perez <fabrimotor2026@gmail.com>
2026-07-16 10:11:16 +0200

    RC2.8.5.1 estable

--- src/components/modals/VisualHelpModal.jsx
+++ src/components/modals/VisualHelpModal.jsx
@@ -1,6 +1,6 @@
 import { X } from "lucide-react";
 import { Button } from "../ui/button";
-import { VISUAL_HELP_IMAGES } from "../../modules/visualHelp/visualHelpImages";
+import { VISUAL_HELP_IMAGES } from "../../modules/visualHelp/visualHelpImages.js";
 
 export default function VisualHelpModal({ item, onClose }) {
   const showVideo = item?.id === "c30" || item?.id === "c40";
