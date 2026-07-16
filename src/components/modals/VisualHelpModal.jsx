ae2ca2cf343305ae010a5ab02f48ee2365d5b78b
Javier Perez <fabrimotor2026@gmail.com>
2026-07-16 11:22:41 +0200

    Fix VisualHelpImages import casing

--- src/components/modals/VisualHelpModal.jsx
+++ src/components/modals/VisualHelpModal.jsx
@@ -1,6 +1,6 @@
 import { X } from "lucide-react";
 import { Button } from "../ui/button";
-import { VISUAL_HELP_IMAGES } from "../../modules/visualHelp/visualHelpImages.js";
+import { VISUAL_HELP_IMAGES } from "../../modules/visualHelp/VisualHelpImages.js";
 
 export default function VisualHelpModal({ item, onClose }) {
   const showVideo = item?.id === "c30" || item?.id === "c40";
